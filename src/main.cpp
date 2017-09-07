bc83a2418130753a2c8eec26cc723861385af2a0
liuxveyang <algouage@gmail.com>
2017-09-07 12:28:53 +0800

    make two changes on feature b
    1. output messages in while/for loop
    2. remove two

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@ int main(int argc, char *argv[argc])
   int i;
 
   for (i = 0; i < 1010; ++i) {
-    // TODO
+    // change (1) of feature b
+    cout << "feature b: for i = " << i << "\n";
   }
 
   // ====================
 
   while (i > 0) {
-    // TODO
+    // change (2) of feture b
+    cout << "feature b: while i = " << i << "\n";
   }
 
   return 0;
