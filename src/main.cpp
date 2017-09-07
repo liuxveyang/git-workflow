68c2ee97c1ad7ec0877fdb6f714ed2dccac86d6c
liuxveyang <algouage@gmail.com>
2017-09-07 12:26:57 +0800

    made two changes on src/main.cpp
    1. output messages in for/while loop
    2. remove two TODOs

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@ int main(int argc, char *argv[argc])
   int i;
 
   for (i = 0; i < 1010; ++i) {
-    // TODO
+    // change (1) on feature A
+    cout << "feture A" << "\n";
   }
 
   // ====================
 
   while (i > 0) {
-    // TODO
+    // change (2) on feature A
+    cout << "feature a" << "\n";
   }
 
   return 0;
