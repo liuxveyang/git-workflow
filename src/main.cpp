d7d2709206c727f4c1c41f16bd8f180e08b2207d
liuxveyang <algouage@gmail.com>
2017-09-07 12:21:02 +0800

    add src directory
    1. create src/main.cpp file
    2. add for and while loop

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char *argv[argc])
+{
+  int i;
+
+  for (i = 0; i < 1010; ++i) {
+    // TODO
+  }
+
+  // ====================
+
+  while (i > 0) {
+    // TODO
+  }
+
+  return 0;
+}
